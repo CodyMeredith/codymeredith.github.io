16a4282fba5cca70801ed162c7bc8f205e0a8e05
CodyMeredith <meredith.cody@gmail.com>
2021-11-13 17:24:58 -0700

    Updated date format in footer

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -10,9 +10,7 @@ try {
         day: "numeric",
         month: "long",
         year: "numeric",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit"
+        
     };
     document.getElementById("last_updated").textContent = new Date().toLocaleDateString("en-US", options);
 } catch (e) {
