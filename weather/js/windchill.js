b0efa727103611277f1ae733c26f9b8b61387a17
CodyMeredith <meredith.cody@gmail.com>
2021-11-20 22:13:20 -0700

    added conditions to the windchill calculation

--- weather/js/windchill.js
+++ weather/js/windchill.js
@@ -3,6 +3,7 @@
 let temperature = document.getElementById("currentTemp").innerText;
 let wind = document.getElementById("windSpeed").innerText;
 
-f = Math.round(35.74 + 0.6215 * temperature - 35.75 * (Math.pow(wind, 0.16)) + 0.4275 * temperature * (Math.pow(wind, 0.16)));
-
-document.getElementById("windChill").innerText = f;
+if (temperature <= 50 && wind >= 4.8) {
+    f = Math.round(35.74 + 0.6215 * temperature - 35.75 * (Math.pow(wind, 0.16)) + 0.4275 * temperature * (Math.pow(wind, 0.16)));
+    document.getElementById("windChill").innerText = f;
+}
