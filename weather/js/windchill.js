804185e42ec17aec69eda3b140418138263dc88e
CodyMeredith <meredith.cody@gmail.com>
2021-11-20 20:30:46 -0700

    Added link to the newest assignment

--- weather/js/windchill.js
+++ weather/js/windchill.js
@@ -5,6 +5,4 @@ let wind = document.getElementById("windSpeed").innerText;
 
 f = Math.round(35.74 + 0.6215 * temperature - 35.75 * (Math.pow(wind, 0.16)) + 0.4275 * temperature * (Math.pow(wind, 0.16)));
 
-// console.log(temperature, wind);
-
 document.getElementById("windChill").innerText = f;
