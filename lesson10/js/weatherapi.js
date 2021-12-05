579d817be4ac9170c869d5078a2f246cd38d53dd
CodyMeredith <meredith.cody@gmail.com>
2021-12-04 22:19:00 -0700

    Created Lesson10 folder, preston.html page, and associated files

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,13 +1,53 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=9afc0f107923b2b648c5af033cf74905&units=imperial"
+const weatherURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=9afc0f107923b2b648c5af033cf74905&lang=en&units=imperial"
 
-fetch(apiURL)
+fetch(weatherURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-    const desc = jsObject.weather[0].description;  // note how we reference the weather array
-    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-    document.getElementById('icon').setAttribute('alt', desc);
+    document.getElementById('currentConditions').innerText = jsObject.weather[0].description;
+    document.getElementById('currentTemp').innerText = Math.round(jsObject.main.temp);
+    document.getElementById('windSpeed').innerText = jsObject.wind.speed;
+    document.getElementById('humidity').innerText = jsObject.main.humidity;
+    // const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    // const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    // document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    // document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    // document.getElementById('icon').setAttribute('alt', desc);
   });
+
+  let temperature = document.getElementById("currentTemp").innerText;
+  let wind = document.getElementById("windSpeed").innerText;
+  
+  if (temperature <= 50 && wind >= 4.8) {
+      f = Math.round(35.74 + 0.6215 * temperature - 35.75 * (Math.pow(wind, 0.16)) + 0.4275 * temperature * (Math.pow(wind, 0.16)));
+      document.getElementById("windChill").innerText = f.text('\u00B0'); }
+      else {
+          document.getElementById("windChill").innerText = "N/A"
+      }
+
+const forecastURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=9afc0f107923b2b648c5af033cf74905&lang=en&units=imperial"
+
+fetch(forecastURL)
+.then((response) => response.json())
+.then((jsonObject) => {
+    console.log(jsonObject);
+
+    const forecast = [];
+    
+    for (let i = 0; i < jsonObject.length; i++ ) {
+        if ("18:00:00" in jsonObject) {
+            forecast.push(jsonObject);
+            console.log(forecast);
+            // document.getElementById('forecastDay1').textContent = jsonObject.list[0].clouds.dt_txt;
+        }
+        
+    };
+
+    
+
+    // const imagesrc = 'https://openweathermap.org/img/w/' + jsonObject.weather[0].icon + '.png';  // note the concatenation
+    // const desc = jsonObject.weather[0].description;  // note how we reference the weather array
+    // document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    // document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    // document.getElementById('icon').setAttribute('alt', desc);
+});
