2533433a34f989a28ce8222dbb5423892edf068d
CodyMeredith <meredith.cody@gmail.com>
2021-12-13 22:46:25 -0700

    created all pages, began css styling, added some js

--- lesson11/js/scripts.js
+++ lesson11/js/scripts.js
@@ -32,8 +32,8 @@ var btns = header.getElementsByClassName("navList");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
   var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
+  current[0].className = current[0].className.replace("active", "");
+  this.className += "active";
   });
 }
 
